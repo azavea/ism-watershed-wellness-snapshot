2bde0f1ffdfe1d1f67d7d2c75630a848eb679561
Casey Cesari <ccesari@azavea.com>
2019-01-07 14:08:28 -0500

    Remove remaining demo code
    This code is no longer needed.
    
    Refs #18

--- src/app/src/App.js
+++ src/app/src/App.js
@@ -1,21 +1,9 @@
 import React, { Component } from 'react';
-import { number } from 'prop-types';
-import { connect } from 'react-redux';
 
 import GLMap from './Map';
 import './App.scss';
-import { updateCount } from './actions';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.inc = this.inc.bind(this);
-    }
-
-    inc() {
-        updateCount();
-    }
-
     render() {
         return (
             <div className='App'>
@@ -25,12 +13,4 @@ class App extends Component {
     }
 }
 
-App.propTypes = {
-    n: number.isRequired,
-};
-
-function mapStateToProps(state) {
-    return state;
-}
-
-export default connect(mapStateToProps)(App);
+export default App;
--- src/app/src/actions.js
+++
@@ -1,3 +0,0 @@
-import { createAction } from 'redux-act';
-
-export const updateCount = createAction('Update count');
