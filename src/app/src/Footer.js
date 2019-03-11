70f26cf31c41d24b9183cb450a61aff860b769d2
Casey Cesari <ccesari@azavea.com>
2019-03-11 13:06:46 -0400

    Fix footer copy placement
    The text should be within the p element.

--- src/app/src/Footer.js
+++ src/app/src/Footer.js
@@ -26,8 +26,7 @@ export default function Footer({ sensorReadingDates }) {
                         alt='Feed icon'
                     />
                 </div>
-                <p className='footer__text' />
-                {message}
+                <p className='footer__text'>{message}</p>
             </div>
         </footer>
     );
